e9aabc1f67da402c55fecadbe834ac4eae38131d
Rachel Fletcher <rafletcher93@gmail.com>
2021-01-23 11:01:12 +0000

    Part 1 complete

--- saturday_homework.py
+++ saturday_homework.py
@@ -1,1 +1,10 @@
+first_name = "Rachel"
+last_name = "Fletcher"
+print(first_name + " " + last_name)
 
+names = [first_name, last_name]
+print(names[0])
+
+register = {"first": first_name, "last": last_name}
+print(register["first"])
+print(register["last"])
