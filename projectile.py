27d180d2c269c4bff5489e0fdf9e2492c4bdad70
Catriona Kemp <c.j.kemp@outlook.com>
2021-01-23 12:06:53 +0000

    part 3 complete

--- projectile.py
+++ projectile.py
@@ -1,1 +1,14 @@
 import math
+from math import pi, tan, cos
+
+g = 9.81
+v = 44
+theta = 80 * (pi/180)
+x = 0.5
+y0 = 1
+
+y = y0 + x * tan(theta) - ((g * x**2) / (2 * (v*cos(theta))**2))
+print(y)
+
+
+
